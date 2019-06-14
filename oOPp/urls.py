687006634c1732b6b9a547f9b78976ceb957edcf
Seiji Nakaya <snakaya@loosedays.jp>
2019-06-14 09:54:48 +0900

    change Project name

--- oOPp/urls.py
+++ oOPp/urls.py
@@ -1,4 +1,4 @@
-"""oOPp URL Configuration
+"""oidcOPprobe URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
